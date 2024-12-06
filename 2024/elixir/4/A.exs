48c68bd3079175da1e86d1d703c47593cac2b706
Adam Higgins <guitarislife87@gmail.com>
2024-12-05 23:07:16 -0600

    Convert to 2024 4A to crazy visual pattern matching

--- 2024/elixir/4/A.exs
+++ 2024/elixir/4/A.exs
@@ -13,40 +13,126 @@ end
       pos = Enum.at(x, j)
       case pos do
         "X" ->
-          surrounding = for di <- -3..3, dj <- -3..3, into: [], do: Enum.at(Enum.at(xs, i + di, []), j + dj, :none)
+            surrounding = Enum.reduce(-3..3, [], fn di, acc ->
+              row = Enum.reduce(-3..3, [], fn dj, row_acc ->
+                cond do
+                  i + di < 0 -> [:nil | row_acc]
+                  j + dj < 0 -> [:nil | row_acc]
+                  i + di >= length(xs) -> [:nil | row_acc]
+                  j + dj >= length(Enum.at(xs, i + di, [])) -> [:nil | row_acc]
+                  true -> [Enum.at(Enum.at(xs, i + di, []), j + dj, :nil) | row_acc]
+                end
+              end) |> Enum.reverse()
+              [row | acc]
+            end) |> Enum.reverse()
+          IO.inspect(surrounding)
           count = 0
           # Left
-          count = cond
-            [_,_,_,["S","A","M","X"],_,_,_] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+             [
+               [ _ , _ , _ , _ , _ , _ ,_ ],
+               [ _ , _ , _ , _ , _ , _ ,_ ],
+               [ _ , _ , _ , _ , _ , _ ,_ ],
+               ["S","A","M","X", _ , _ ,_ ],
+               [ _ , _ , _ , _ , _ , _ ,_ ],
+               [ _ , _ , _ , _ , _ , _ ,_ ],
+               [ _ , _ , _ , _ , _ , _ ,_ ]
+             ] -> count + 1
+             _ -> count
+          end
           # Right
-          count = cond
-            [_,_,_,[_,_,_,"X","M","A","S"],_,_,_] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ ,"X","M","A","S"],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+            end
           # Up
-          count = cond
-            [[_,_,_,"S"],[_,_,_,"A"],[_,_,_,"M"],[_,_,_,"X"],_,_,_] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              [ _ , _ , _ ,"S", _ , _ , _ ],
+              [ _ , _ , _ ,"A", _ , _ , _ ],
+              [ _ , _ , _ ,"M", _ , _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+          end
           # Down
-          count = cond
-            [_,_,_,[_,_,_,"X"],[_,_,_,"M"],[_,_,_,"A"],[_,_,_,"S"]] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ , _ ,"M", _ , _ , _ ],
+              [ _ , _ , _ ,"A", _ , _ , _ ],
+              [ _ , _ , _ ,"S", _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+            end
           # Diagonal Up Left
-          count = cond
-            [["S"],[_,"A"],[_,_,"M"],[_,_,_,"X"],_,_,_] in surrounding -> count + 1
-            true -> count
-          # Diagonal Down Left
-          count = cond
-            [_,_,_,[_,_,_,"X"],[_,_,"M"],[_,"A"],["S"]] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              ["S", _ , _ , _ , _ , _ , _ ],
+              [ _ ,"A", _ , _ , _ , _ , _ ],
+              [ _ , _ ,"M", _ , _ , _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+          end
           # Diagonal Up Right
-          count = cond
-            [[_,_,_,_,_,_,"S"],[_,_,_,_,_,"A"],[_,_,_,_,"M"],[_,_,_,"X"]] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              [ _ , _ , _ , _ , _ , _ ,"S"],
+              [ _ , _ , _ , _ , _ ,"A", _ ],
+              [ _ , _ , _ , _ ,"M", _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+          end
+          # Diagonal Down Left
+          count = case surrounding do
+            [
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ ,"M", _ , _ , _ , _ ],
+              [ _ ,"A", _ , _ , _ , _ , _ ],
+              ["S", _ , _ , _ , _ , _ , _ ]
+            ] -> count + 1
+            _ -> count
+            end
           # Diagonal Down Right
-          count = cond
-            [_,_,_,[_,_,_,"X"],[_,_,_,_,"M"],[_,_,_,_,_,"A"],[_,_,_,_,_,_,"S"]] in surrounding -> count + 1
-            true -> count
+          count = case surrounding do
+            [
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ , _ , _ , _ , _ ],
+              [ _ , _ , _ ,"X", _ , _ , _ ],
+              [ _ , _ , _ , _ ,"M", _ , _ ],
+              [ _ , _ , _ , _ , _ ,"A", _ ],
+              [ _ , _ , _ , _ , _ , _ ,"S"]
+            ] -> count + 1
+            _ -> count
+            end
+          IO.inspect(count)
+          count
         _ -> 0
       end
     end) |> Enum.sum()
