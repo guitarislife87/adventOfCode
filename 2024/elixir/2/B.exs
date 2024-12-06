4b0e677e10d9166791c8e4ffef038545b7c00a9a
Adam Higgins <guitarislife87@gmail.com>
2024-12-05 21:31:11 -0600

    Fix 2024 2B

--- 2024/elixir/2/B.exs
+++ 2024/elixir/2/B.exs
@@ -7,9 +7,16 @@ end
 |> String.split("\n")
 |> Enum.map(fn line ->
   nums = line |> String.split(" ") |> Enum.map(&String.trim/1) |> Enum.map(&String.to_integer/1)
-  Enum.zip(nums, tl(nums))
+  Enum.reduce(0..length(nums) - 1, [], fn i, acc ->
+    temp_nums = List.delete_at(nums, i)
+    acc ++ [Enum.zip(temp_nums, tl(temp_nums))]
+  end)
 end)
-|> Enum.filter(fn a -> Enum.all?(a, fn {x, y} -> x < y end) or Enum.all?(a, fn {x, y} -> x > y end) end)
-|> Enum.filter(fn a -> Enum.all?(a, fn {x, y} -> abs(y - x) < 4 end) end)
+|> IO.inspect()
+|> Enum.filter(fn b ->
+  Enum.any?(b, fn a ->
+    (Enum.all?(a, fn {x, y} -> x < y end) or Enum.all?(a, fn {x, y} -> x > y end)) and Enum.all?(a, fn {x, y} -> abs(y - x) < 4 end)
+  end)
+end)
 |> Enum.count()
 |> IO.puts()
