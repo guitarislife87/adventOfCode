cd428db3f0646be45efd42e358fea77697cafd6b
Adam Higgins <guitarislife87@gmail.com>
2024-12-05 22:02:26 -0600

    Refactor 2024 4A to do pattern match instead of all the checks

--- 2024/elixir/4/A.exs
+++ 2024/elixir/4/A.exs
@@ -13,16 +13,40 @@ end
       pos = Enum.at(x, j)
       case pos do
         "X" ->
+          surrounding = for di <- -3..3, dj <- -3..3, into: [], do: Enum.at(Enum.at(xs, i + di, []), j + dj, :none)
           count = 0
-          count = if j > 2 and Enum.at(x, j-1) == "M" and Enum.at(x, j-2) == "A" and Enum.at(x, j-3) == "S", do: count + 1, else: count
-          count = if j < (length(x) - 3) and Enum.at(x, j+1) == "M" and Enum.at(x, j+2) == "A" and Enum.at(x, j+3) == "S", do: count + 1, else: count
-          count = if i > 2 and Enum.at(Enum.at(xs, i-1), j) == "M" and Enum.at(Enum.at(xs, i-2), j) == "A" and Enum.at(Enum.at(xs, i-3), j) == "S", do: count + 1, else: count
-          count = if i < (length(xs) - 3) and Enum.at(Enum.at(xs, i+1), j) == "M" and Enum.at(Enum.at(xs, i+2), j) == "A" and Enum.at(Enum.at(xs, i+3), j) == "S", do: count + 1, else: count
-          count = if j > 2 and i > 2 and Enum.at(Enum.at(xs, i-1), j-1) == "M" and Enum.at(Enum.at(xs, i-2), j-2) == "A" and Enum.at(Enum.at(xs, i-3), j-3) == "S", do: count + 1, else: count
-          count = if j < (length(x) - 3) and i > 2 and Enum.at(Enum.at(xs, i-1), j+1) == "M" and Enum.at(Enum.at(xs, i-2), j+2) == "A" and Enum.at(Enum.at(xs, i-3), j+3) == "S", do: count + 1, else: count
-          count = if j > 2 and i < (length(xs) - 3) and Enum.at(Enum.at(xs, i+1), j-1) == "M" and Enum.at(Enum.at(xs, i+2), j-2) == "A" and Enum.at(Enum.at(xs, i+3), j-3) == "S", do: count + 1, else: count
-          count = if j < (length(x) - 3) and i < (length(xs) - 3) and Enum.at(Enum.at(xs, i+1), j+1) == "M" and Enum.at(Enum.at(xs, i+2), j+2) == "A" and Enum.at(Enum.at(xs, i+3), j+3) == "S", do: count + 1, else: count
-          count
+          # Left
+          count = cond
+            [_,_,_,["S","A","M","X"],_,_,_] in surrounding -> count + 1
+            true -> count
+          # Right
+          count = cond
+            [_,_,_,[_,_,_,"X","M","A","S"],_,_,_] in surrounding -> count + 1
+            true -> count
+          # Up
+          count = cond
+            [[_,_,_,"S"],[_,_,_,"A"],[_,_,_,"M"],[_,_,_,"X"],_,_,_] in surrounding -> count + 1
+            true -> count
+          # Down
+          count = cond
+            [_,_,_,[_,_,_,"X"],[_,_,_,"M"],[_,_,_,"A"],[_,_,_,"S"]] in surrounding -> count + 1
+            true -> count
+          # Diagonal Up Left
+          count = cond
+            [["S"],[_,"A"],[_,_,"M"],[_,_,_,"X"],_,_,_] in surrounding -> count + 1
+            true -> count
+          # Diagonal Down Left
+          count = cond
+            [_,_,_,[_,_,_,"X"],[_,_,"M"],[_,"A"],["S"]] in surrounding -> count + 1
+            true -> count
+          # Diagonal Up Right
+          count = cond
+            [[_,_,_,_,_,_,"S"],[_,_,_,_,_,"A"],[_,_,_,_,"M"],[_,_,_,"X"]] in surrounding -> count + 1
+            true -> count
+          # Diagonal Down Right
+          count = cond
+            [_,_,_,[_,_,_,"X"],[_,_,_,_,"M"],[_,_,_,_,_,"A"],[_,_,_,_,_,_,"S"]] in surrounding -> count + 1
+            true -> count
         _ -> 0
       end
     end) |> Enum.sum()
